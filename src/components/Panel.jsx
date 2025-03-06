ce22a6822c2a0112c7fe97950d7198c21858af1e
Marian Halibei <137928134+mar1anDEV@users.noreply.github.com>
2025-03-06 19:46:18 +0100

    Update Panel.jsx

--- src/components/Panel.jsx
+++ src/components/Panel.jsx
@@ -1,13 +1,30 @@
-import React from 'react'
-import FormInput from './FormInput'
-import FormOutput from './FormOutput'
+import React, { useState } from 'react';
+import FormInput from './FormInput';
+import FormOutput from './FormOutput';
+import PanelLoading from './panelLoading';
 
 function Panel() {
+  const [panelLoading, setPanelLoading] = useState(false);
+  const [showOutput, setShowOutput] = useState(false);
+
+  const handleLoading = (isLoading) => {
+    setPanelLoading(isLoading);
+  };
+
+  const handleShowOutput = (show) => {
+    setShowOutput(show);
+  };
+
   return (
     <div className="PanelContainerMain" style={{ position: 'absolute', height: '100%', width: '100%', top: 0 }}>
-                <FormInput></FormInput>
-            </div>
-  )
+      {panelLoading && <PanelLoading />}
+      <FormInput 
+        onSetLoading={handleLoading} 
+        onSetShowOutput={handleShowOutput} 
+      />
+      {showOutput && <FormOutput />}
+    </div>
+  );
 }
 
-export default Panel
+export default Panel;
